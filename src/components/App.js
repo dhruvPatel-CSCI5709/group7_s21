0920390639bfd5a9bc63be1c7ef18fbc23e2ed95
FMakadia <fl410364@dal.ca>
2021-06-23 15:08:34 +0530

    Login Error Redirect Solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Route, useHistory } from 'react-router-dom'
 import 'antd/dist/antd.css'
 import DeleteProfile from './deleteProfile/deleteProfile'
@@ -8,6 +8,7 @@ import Profile from './profile/Profile'
 import Dashboard from './dashboard/Dashboard'
 import Reports from './reports/Reports'
 import Expense from './expense/Expense'
+import Login from './login/LoginPage'
 
 export default function PermanentDrawerLeft() {
   const history = useHistory()
@@ -20,18 +21,19 @@ export default function PermanentDrawerLeft() {
   }, [])
 
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/deleteProfile' component={DeleteProfile}></Route>
-        <Route path='/profile' component={Profile}></Route>
-        <Route path='/homePage' component={HomePage}></Route>
-        <Route path='/dashboard' component={Dashboard}></Route>
-        <Route path='/incorrectPassword' component={IncorrectPassword}></Route>
-        <Route path='/reports' component={Reports}></Route>
-        <Route path='/expense' component={Expense}></Route>
-        <Route path='/login' component={Login}></Route>
-      </div>
-    </BrowserRouter>
+    <div className='App'>
+      <Route exact path='/login' component={Login}></Route>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/deleteProfile' component={DeleteProfile}></Route>
+      <Route exact path='/profile' component={Profile}></Route>
+      <Route exact path='/dashboard' component={Dashboard}></Route>
+      <Route
+        exact
+        path='/incorrectPassword'
+        component={IncorrectPassword}
+      ></Route>
+      <Route exact path='/reports' component={Reports}></Route>
+      <Route exact path='/expense' component={Expense}></Route>
+    </div>
   )
 }
