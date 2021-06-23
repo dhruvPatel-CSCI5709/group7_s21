eb66524b49bf3a422bfd1f5c84d1077202217cfc
nikunjdhola <nk309459@dal.ca>
2021-06-23 15:37:26 +0530

    logout icon updated

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,84 +1,86 @@
-import React from 'react'
-import DashboardTwoToneIcon from '@material-ui/icons/DashboardTwoTone'
-import AccountBoxRoundedIcon from '@material-ui/icons/AccountBoxRounded'
-import NotificationsNoneTwoToneIcon from '@material-ui/icons/NotificationsNoneTwoTone'
-import MonetizationOnTwoToneIcon from '@material-ui/icons/MonetizationOnTwoTone'
-import AssessmentIcon from '@material-ui/icons/Assessment'
-import { useHistory } from 'react-router-dom'
-import './Sidebar.css'
-import logout from '../../reusables/Logout'
+import React from "react";
+import DashboardTwoToneIcon from "@material-ui/icons/DashboardTwoTone";
+import AccountBoxRoundedIcon from "@material-ui/icons/AccountBoxRounded";
+import NotificationsNoneTwoToneIcon from "@material-ui/icons/NotificationsNoneTwoTone";
+import MonetizationOnTwoToneIcon from "@material-ui/icons/MonetizationOnTwoTone";
+import AssessmentIcon from "@material-ui/icons/Assessment";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { useHistory } from "react-router-dom";
+import "./Sidebar.css";
+import logout from "../../reusables/Logout";
 
 const lists = [
   {
     id: 1,
-    title: 'Dashboard',
-    route: 'dashboard',
+    title: "Dashboard",
+    route: "dashboard",
     icon: <DashboardTwoToneIcon />,
   },
   {
     id: 2,
-    title: 'Finances',
-    route: 'finances',
+    title: "Finances",
+    route: "finances",
     icon: <MonetizationOnTwoToneIcon />,
   },
   {
     id: 3,
-    title: 'Notifications',
-    route: 'notifications',
+    title: "Notifications",
+    route: "notifications",
     icon: <NotificationsNoneTwoToneIcon />,
   },
   {
     id: 4,
-    title: 'Profile',
-    route: 'profile',
+    title: "Profile",
+    route: "profile",
     icon: <AccountBoxRoundedIcon />,
   },
   {
     id: 5,
-    title: 'Statistical Reports',
-    route: 'reports',
+    title: "Statistical Reports",
+    route: "reports",
     icon: <AssessmentIcon />,
   },
   {
     id: 6,
-    title: 'Logout',
-    route: null,
+    title: "Expense",
+    route: "expense",
+    icon: <AssessmentIcon />,
   },
   {
     id: 7,
-    title: 'Expense',
-    route: 'expense',
-    icon: <AssessmentIcon />,
+    title: "Logout",
+    route: null,
+    icon: <ExitToAppIcon />,
   },
-]
+];
 
 export default function Sidebar() {
-  const history = useHistory()
+  const history = useHistory();
 
   const handleRoute = (title, route) => {
-    if (title == 'Logout') {
-      logout()
-      history.push('/login')
-      return
+    if (title == "Logout") {
+      logout();
+      history.push("/login");
+      return;
     }
-    let path = '/' + route
-    history.push(path, { title: title })
-  }
+    let path = "/" + route;
+    history.push(path, { title: title });
+  };
 
-  const path = history.location.pathname.substr(1)
-  console.log(path)
+  const path = history.location.pathname.substr(1);
+  console.log(path);
   return (
-    <div className='root-sidebar'>
+    <div className="root-sidebar">
       {lists.map((list) => (
         <button
           key={list.id}
-          className='button-dashboard'
+          className="button-dashboard"
           style={{
-            backgroundColor: list.title === path ? '#2a95bf' : '',
-            color: list.title === path ? 'white' : '#808080',
+            backgroundColor: list.title === path ? "#2a95bf" : "",
+            color: list.title === path ? "white" : "#808080",
           }}
           onClick={() => {
-            handleRoute(list.title, list.route)
+            handleRoute(list.title, list.route);
           }}
         >
           {list.icon}
@@ -86,5 +88,5 @@ export default function Sidebar() {
         </button>
       ))}
     </div>
-  )
+  );
 }
