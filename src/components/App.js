252417e90649172c855dc4867aefca596a683865
Jemis Rameshbhai Zadafiya <jm900770@dal.ca>
2021-06-23 23:16:42 +0530

    merge emicalc

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Route, useHistory } from 'react-router-dom'
+import { BrowserRouter, Route, useHistory } from 'react-router-dom'
 import 'antd/dist/antd.css'
 import DeleteProfile from './deleteProfile/deleteProfile'
 import HomePage from './homePage/HomePage'
@@ -9,13 +9,7 @@ import Dashboard from './dashboard/Dashboard'
 import Reports from './reports/Reports'
 import Expense from './expense/Expense'
 import Login from './login/LoginPage'
-<<<<<<< HEAD
 import EmiCalculator  from './emicalculator/EmiCalculator'
-=======
-import SetUpEMIDueDate from './notification/emi_setup'
-import Register from './register/RegisterPage'
-
->>>>>>> 8d9a89093343e4e4e0ff896f0763d32b60a62dcc
 export default function App() {
   const history = useHistory()
 
@@ -30,7 +24,6 @@ export default function App() {
   return (
     <div className='App'>
       <Route exact path='/login' component={Login}></Route>
-      <Route exact path='/register' component={Register}></Route>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/deleteProfile' component={DeleteProfile}></Route>
       <Route exact path='/profile' component={Profile}></Route>
@@ -42,11 +35,7 @@ export default function App() {
       ></Route>
       <Route exact path='/reports' component={Reports}></Route>
       <Route exact path='/expense' component={Expense}></Route>
-<<<<<<< HEAD
       <Route exact path='/emicalculator' component={EmiCalculator}></Route>
-=======
-      <Route exact path='/notifications' component={SetUpEMIDueDate} />
->>>>>>> 8d9a89093343e4e4e0ff896f0763d32b60a62dcc
     </div>
   )
 }
