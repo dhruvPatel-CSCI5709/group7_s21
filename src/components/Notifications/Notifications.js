c32ced294f5e5a9fe24d03a651f40386ae3bfab3
dhruvPatel-CSCI5709 <84716210+dhruvPatel-CSCI5709@users.noreply.github.com>
2021-07-21 02:32:37 -0300

    Revert "Dhruv"

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -2,8 +2,8 @@ import { notification } from "antd";
 
 const Notification = (type, message, description = "") => {
   return notification[type]({
-    message: message.toString(),
-    description: description.toString(),
+    message: message,
+    description: description,
   });
 };
 
